00caf298e5ce3efbb96dbbbe3abad25e546837db
Simon Elliott <simon.elliott@hashicorp.com>
2024-11-04 20:37:56 +0000

    quick renamed of subnet_prefix

--- main.tf
+++ main.tf
@@ -9,20 +9,11 @@ resource "azurerm_virtual_network" "main" {
     for_each = zipmap(var.subnet_name, var.subnet_cidr)
     content {
       name           = subnet.key
-      address_prefix = subnet.value
+      address_prefixes = subnet.value
     }
   }
 
   tags = merge(
     local.common_tags, var.extra_tags,
   )
-}
-
-
-resource "azurerm_subnet" "bastion" {
-  count                = local.enable_bastion
-  name                 = "AzureBastionSubnet"
-  resource_group_name  = data.azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = var.bastion_subnet_address
 }
