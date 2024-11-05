69179f56b7ec21133610f009c398b4d134f9efac
Simon Elliott <simon.elliott@hashicorp.com>
2024-11-05 12:43:10 +0000

    develop - removed bastion

--- data.tf
+++ data.tf
@@ -8,9 +8,7 @@ locals {
     subnet_cidr    = var.subnet_cidr
     security_group = var.security_group
   }]
-
-  enable_bastion = var.enable_bastion == true ? 1 : 0
-
+  
   common_tags = tomap({
     Environment = var.environment
     Terraform   = "true"
